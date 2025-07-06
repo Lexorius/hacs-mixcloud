78116db328dde0a544929458a5cf0e2a6697286e
lexorius <thomas+github@kloppholz.de>
2025-07-06 10:37:33 +0200

    messing with init

--- custom_components/mixcloud/__init__.py
+++ custom_components/mixcloud/__init__.py
@@ -6,9 +6,8 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN, API_BASE_URL
+from .const import DOMAIN
 
-PLATFORM = "sensor"
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -16,7 +15,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     username = entry.data["username"]
     session = async_get_clientsession(hass)
-
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = {
         "username": username,
@@ -25,13 +23,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     _LOGGER.debug("Setting up Mixcloud entry for user '%s'", username)
 
-    # KORREKTE SYNTAX!
-    await hass.config_entries.async_forward_entry_setups(PLATFORM, [entry])
+    # **GENAU SO!**
+    await hass.config_entries.async_forward_entry_setups("sensor", [entry])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, [PLATFORM])
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id, None)
     _LOGGER.debug("Unloaded Mixcloud entry for user '%s'", entry.data["username"])
